fa620d7de2b6697c54bbe684696e9ec3ff3d448c
acgo <128870982+acgogo2023@users.noreply.github.com>
2024-07-25 16:24:34 +0800

    Update install_frp.sh

--- install_frp.sh
+++ install_frp.sh
@@ -12,8 +12,17 @@ tar -xzvf frp_0.59.0_linux_amd64.tar.gz
 cd frp_0.59.0_linux_amd64
 
 # 提示用户输入server_addr
-read -p "Please enter the server address (server_addr): " SERVER_ADDR
-read -p "Please enter the server port (server_port): " SERVER_PORT
+read -p "Please enter the server address (server_addr) [服务端的IP]: " SERVER_ADDR
+read -p "Please enter the server port (server_port) [默认7000]: " SERVER_PORT
+
+# 如果用户没有输入server_port，使用默认值7000
+if [ -z "$SERVER_PORT" ]; then
+  SERVER_PORT=7000
+fi
+
+# 提示用户输入local_port和remote_port
+read -p "Please enter the local port (local_port): " LOCAL_PORT
+read -p "Please enter the remote port (remote_port): " REMOTE_PORT
 
 # 更新frpc.toml文件
 cat > frpc.toml <<EOL
@@ -25,10 +34,19 @@ server_port = $SERVER_PORT
 type = tcp
 plugin = http_proxy
 local_ip = 127.0.0.1
-local_port = 5269
-remote_port = 5269
+local_port = $LOCAL_PORT
+remote_port = $REMOTE_PORT
 EOL
 
 # 提示用户配置完成
-echo "FRP configuration is complete. You can start FRP using the following command:"
-echo "./frpc -c ./frpc.toml"
+echo "FRP configuration is complete."
+
+# 提示用户是否运行FRP
+read -p "Do you want to start FRP now? (yes/no): " RUN_FRP
+
+if [ "$RUN_FRP" = "yes" ]; then
+  ./frpc -c ./frpc.toml
+else
+  echo "You can start FRP later using the following command:"
+  echo "./frpc -c ./frpc.toml"
+fi
